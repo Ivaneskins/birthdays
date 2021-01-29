fbd3d11a6724812ac7b458c789d441fa3abcbfa5
Ivan <weina2004@mail.ru>
2021-01-29 13:33:24 +0500

    delete white spaces

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => { 
     
     let birthdays = {
         'Ivan Vas':'1987-10-20'
     };
 
     let unoYear = Date.parse('2020-01-01') - Date.parse('2019-01-01'); // 31536000000 milSec per Year
+
+    let my = new Date(birthdays['Ivan Vas']); // My birthday 
     
 
-    const now = new Date(); // Date Today 
-    const my = new Date(birthdays['Ivan Vas']); // My birthday    
+    let form = document.querySelector('form'),
+        formBirthday = form.querySelector("input[type='date']"),
+        formBtnSend = form.querySelector("input[type='submit']"),
+        formText = form.querySelector("input[type='text']");
+    
+        formBtnSend.addEventListener('click', evt => {
+            evt.preventDefault();           
+            
+            my = new Date(formBirthday.value);
+            console.log(my);            
+            setCloak('.timer', my);
+            
+    });  
 
-    function leftUntilBirthday(birthday, datetoday) {
-        let remain = Date.parse(birthday) + (datetoday.getFullYear() - birthday.getFullYear()) * unoYear  - Date.parse(datetoday);
-        if (datetoday.getMonth() > birthday.getMonth()) { 
+    function leftUntilBirthday(birthday) {
+        let today = new Date();
+        
+        let remain = Date.parse(birthday) + (today.getFullYear() - birthday.getFullYear()) * unoYear  - Date.parse(today);
+        if (today.getMonth() > birthday.getMonth()) { 
             remain += unoYear;                                          
         }
-        console.log(datetoday);         
+        // console.log(today);         
         let days = Math.floor((remain / 1000 / 60 / 60 / 24)),
             hours = Math.floor((remain / 1000 / 60 / 60) % 24),
             minutes = Math.floor((remain / 1000 / 60) % 60),
@@ -30,28 +45,28 @@ window.addEventListener('DOMContentLoaded', () => {
             'seconds': seconds
             };  
         }    
-        
-    function setCloak(selector, birthday) {
+    function setCloak(selector, birthday) {        
+            
         const timer = document.querySelector(selector),
               days = timer.querySelector('.card__timer_days'),
               hours = timer.querySelector('.card__timer_hours'),
               minutes = timer.querySelector('.card__timer_minutes'),
               seconds = timer.querySelector('.card__timer_seconds'), 
               timerInterval = setInterval(updateCloak, 1000); 
-              updateCloak();
+              updateCloak();             
               
         function updateCloak() {
-            const t = leftUntilBirthday(birthday, new Date());
-            days.textContent = t.days;
+            const t = leftUntilBirthday(birthday);
+            days.innerHTML = t.days;
             hours.textContent = t.hours;
             minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
-                              
+            seconds.textContent = t.seconds;                              
         }
-
     }      
 
     setCloak('.timer', my);
+
+
 
      
 
