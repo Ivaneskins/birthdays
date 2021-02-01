7f6744d83cf7e40cc51d23d51b08c35c85a44a93
Ivan <weina2004@mail.ru>
2021-02-01 16:12:31 +0500

    create objs with classes

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ window.addEventListener('DOMContentLoaded', () => {
         'Ivan Vas':'1987-10-20'
     };
 
+    
+
     let unoYear = Date.parse('2020-01-01') - Date.parse('2019-01-01'); // 31536000000 milSec per Year
 
-    let my = new Date(birthdays['Ivan Vas']); // My birthday 
+    // let my = new Date(birthdays['Ivan Vas']); // My birthday 
     
 
     let form = document.querySelector('form'),
@@ -16,55 +18,91 @@ window.addEventListener('DOMContentLoaded', () => {
         formBtnSend = form.querySelector("input[type='submit']"),
         formText = form.querySelector("input[type='text']");
     
+    
         formBtnSend.addEventListener('click', evt => {
-            evt.preventDefault();           
+            evt.preventDefault();
+            if (Object.keys(birthdays).length >= 3) {
+                 alert('Превышен лимит ввода дат');               
+            } 
+            else if (formText.value === '') {
+                alert('введите корректное значение');
+            }
+            // else if (birthdays.key === formText.value){
+            //     alert('Вы ввели same text');
+
+            // }          
+            else {
+                new addEvent(`${formText.value}`, `${formBirthday.value}`).render();            
+                birthdays[formText.value] = formBirthday.value;
+                console.log(Object.keys(birthdays).length);
+                form.reset();
+                console.log(Object.keys(birthdays));              
+            }
             
-            my = new Date(formBirthday.value);
-            console.log(my);            
-            setCloak('.timer', my);
             
+
+                   
     });  
 
-    function leftUntilBirthday(birthday) {
-        let today = new Date();
-        
-        let remain = Date.parse(birthday) + (today.getFullYear() - birthday.getFullYear()) * unoYear  - Date.parse(today);
-        if (today.getMonth() > birthday.getMonth()) { 
-            remain += unoYear;                                          
-        }
-        // console.log(today);         
-        let days = Math.floor((remain / 1000 / 60 / 60 / 24)),
-            hours = Math.floor((remain / 1000 / 60 / 60) % 24),
-            minutes = Math.floor((remain / 1000 / 60) % 60),
-            seconds = Math.floor((remain / 1000) % 60);
-        return {
-            'total': remain,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-            };  
-        }    
-    function setCloak(selector, birthday) {        
-            
-        const timer = document.querySelector(selector),
-              days = timer.querySelector('.card__timer_days'),
-              hours = timer.querySelector('.card__timer_hours'),
-              minutes = timer.querySelector('.card__timer_minutes'),
-              seconds = timer.querySelector('.card__timer_seconds'), 
-              timerInterval = setInterval(updateCloak, 1000); 
-              updateCloak();             
-              
-        function updateCloak() {
-            const t = leftUntilBirthday(birthday);
-            days.innerHTML = t.days;
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;                              
-        }
-    }      
+    function updateClock() {
 
-    setCloak('.timer', my);
+    }
+
+    class addEvent {
+        constructor(name, birthday){
+            this.name = name;
+            this.birthday = new Date(birthday);              
+            
+            let today = new Date();        
+            let remain = Date.parse(this.birthday) + (today.getFullYear() - this.birthday.getFullYear()) * unoYear  - Date.parse(today);
+            if (today.getMonth() > this.birthday.getMonth()) { 
+                remain += unoYear;                                          
+            }                
+            this.days = Math.floor((remain / 1000 / 60 / 60 / 24));
+            this.hours = Math.floor((remain / 1000 / 60 / 60) % 24);
+            this.minutes = Math.floor((remain / 1000 / 60) % 60);
+            this.seconds = Math.floor((remain / 1000) % 60);            
+        }
+
+        render() {
+            const parent = document.querySelector('.wrapper.main');
+            const newBirthday = document.createElement('div');
+            newBirthday.classList.add('birthday');            
+            newBirthday.innerHTML = `
+                <div class="birthday__person">
+                    <p>${this.name}</p>
+                </div>
+                <div class="timer">
+                    <div class="card__timer"> 
+                        <div class="card__timer_days">${this.days}</div>
+                        <div class="wrap__string"></div>
+                        <p>days</p>
+                    </div>
+                    <div class="card__timer"> 
+                        <div class="card__timer_hours">${this.hours}</div>
+                        <div class="wrap__string"></div>
+                        <p>hours</p>
+                    </div>
+                    <div class="card__timer">
+                        <div class="card__timer_minutes">${this.minutes}</div>
+                        <div class="wrap__string"></div>
+                        <p>minutes</p>
+                    </div>
+                    <div class="card__timer">
+                        <div class="card__timer_seconds">${this.seconds}</div>
+                        <div class="wrap__string"></div>
+                        <p>seconds</p>
+                    </div>
+                </div>
+            `;
+            parent.insertAdjacentElement("beforeend", newBirthday);            
+        }  
+
+    }
+    // new addEvent('Ivan', '1987-10-20').render();    
+    // new addEvent('Svetlana', '1991-12-07').render();
+
+
 
 
 
---
+++ js/test.js
@@ -0,0 +1,5 @@
+'use strict';
+// const year = getFullYear('1987-10-20');
+const now = new Date();
+console.log(`now = ${now}`);
+// console.log(year);
